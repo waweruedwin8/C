0822e645ebe2e9e8509f1dec7b64fb4315bc5ff3
waweruedwin8 <waweruedwin8@gmail.com>
2025-01-07 02:57:13 +0300

    created a baisc arithmetic program for addition in c++

---
+++ C++/coding_challenges/sum.cpp
@@ -0,0 +1,20 @@
+//universal multipurpose header in cpp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+       int a,b;
+       int sum = a + b;
+       cout << "Enter two numbers: ";
+
+       // Read input from user
+       cin >> a >> b;
+
+       // Display the sum
+       cout << "Sum: " << sum << endl;
+
+        cout << sqrt(sum)<<endl; //
+        return 0;
+}
+// Above code is a simple C++ program that takes two numbers from the user, adds them, and prints the sum. It also calculates the square root of the sum.
